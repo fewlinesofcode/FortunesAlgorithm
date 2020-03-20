7f186c4eb46951c50208cb282a360ca50fdf4083
Oleksandr Glagoliev <aglagoliev@gmail.com>
2020-03-20 21:33:38 +0200

    Improve SK Demo

--- SpriteKitDemo/SpriteKitDemo/GameScene.swift
+++ SpriteKitDemo/SpriteKitDemo/GameScene.swift
@@ -18,9 +18,13 @@ class GameScene: SKScene {
     var delaunayDiagramNode = SKShapeNode()
     
     private var clippingRect: Rectangle {
-        Rectangle(
-            origin: Site(x: 50, y: 50),
-            size: Size(width: Double(frame.width) - 100, height: Double(frame.height) - 100)
+        let padding: Double = 50
+        return Rectangle(
+            origin: Site(x: padding, y: padding),
+            size: Size(
+                width: Double(frame.width) - padding * 2,
+                height: Double(frame.height) - padding * 2
+            )
         )
     }
         
@@ -42,20 +46,13 @@ class GameScene: SKScene {
             
             let numRepeats = 1
             let radius: CGFloat = 0
+            let unitPadding: CGFloat = 4
             for i in 0..<numRepeats {
-                let paddedHull = paddedPolygon(hullVertices, padding: CGFloat(-i) * 10)
+                let paddedHull = paddedPolygon(hullVertices, padding: CGFloat(-i) * unitPadding)
                 if let path = UIBezierPath.roundedCornersPath(paddedHull, radius) {
                     voronoiPath.append(path)
                 }
             }
-            
-//            let centroid = polygonCentroid(hullVertices)
-//
-//            for vtx in hullVertices {
-//                voronoiPath.move(to: centroid)
-//                voronoiPath.addLine(to: vtx)
-//            }
-            
             
             for n in cell.neighbours() {
                 delaunayPath.move(to: cell.site.cgPoint)
@@ -64,12 +61,12 @@ class GameScene: SKScene {
         }
         voronoiDiagramNode.path = voronoiPath.cgPath
         voronoiDiagramNode.strokeColor = UIColor.black
-        voronoiDiagramNode.lineWidth = 3.0
+        voronoiDiagramNode.lineWidth = 2.0
         voronoiDiagramNode.fillColor = .clear
         
         delaunayDiagramNode.path = delaunayPath.cgPath
         delaunayDiagramNode.strokeColor = UIColor.green
-        delaunayDiagramNode.lineWidth = 1.0
+        delaunayDiagramNode.lineWidth = 2.0
         delaunayDiagramNode.fillColor = .clear
     }
     
@@ -82,23 +79,21 @@ class GameScene: SKScene {
         let lby = offset
         let ubx = Double(view.bounds.width) - 2 * offset
         let uby = Double(view.bounds.height) - 2 * offset
-        let randomPoints = randomSites(50, xRange: lbx..<ubx, yRange: lby..<uby)
-        let r: CGFloat = 20
+        let randomPoints = randomSites(30, xRange: lbx..<ubx, yRange: lby..<uby)
+        let r: CGFloat = 10
         balls = randomPoints.map {
-            let point = SKShapeNode(circleOfRadius: r)
-            point.position = $0.cgPoint
-            point.fillColor = .red
+            let node = SKShapeNode(circleOfRadius: r)
+            node.position = $0.cgPoint
+            node.fillColor = .red
             
             let body = SKPhysicsBody(circleOfRadius: r)
             body.affectedByGravity = false
-            body.linearDamping = 0.9
-            body.mass = 2.0
 
-            point.physicsBody = body
-            point.isHidden = false
+            node.physicsBody = body
+            node.isHidden = false
             
-            addChild(point)
-            return point
+            addChild(node)
+            return node
         }
         
         redraw(Set<Site>(balls.map { $0.position.point }))
@@ -127,44 +122,18 @@ class GameScene: SKScene {
         return set
     }
     
-    
-    var hexLike: [Site] {
-        let num = 5
-        let step: Double = 50
-        var res = [Site]()
-        for i in 0..<num {
-            for j in 0..<3 {
-                res.append(
-                    Site(
-                        x:Double(i) * step + Double(j) * step / 2 + 200,
-                        y:Double(j) * step + 200
-                    )
-                )
-            }
-        }
-        return res
-    }
-    
     func touchDown(atPoint pos : CGPoint) {
     }
     
     func touchMoved(toPoint pos : CGPoint) {
         let spriteAtPoint = atPoint(pos)
-        if spriteAtPoint != voronoiDiagramNode {
-            spriteAtPoint.position = CGPoint(x: pos.x.rounded(), y: pos.y.rounded())
-            
-        }
+        if spriteAtPoint == voronoiDiagramNode || spriteAtPoint == delaunayDiagramNode { return }
+        spriteAtPoint.position = CGPoint(x: pos.x.rounded(), y: pos.y.rounded())
     }
     
     func touchUp(atPoint pos : CGPoint) {
         let ball = atPoint(pos)
-        if ball == voronoiDiagramNode { return }
-        
-        let force = CGVector(
-            dx: CGFloat.random(in: -300...300),
-            dy: CGFloat.random(in: -300...300)
-        )
-        ball.physicsBody?.applyImpulse(force)
+        if ball == voronoiDiagramNode || ball == delaunayDiagramNode { return }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
