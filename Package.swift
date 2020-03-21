c484b86a8a53d3cc525783e6dab756de7843bdec
Oleksandr Glagoliev <aglagoliev@gmail.com>
2020-03-21 09:27:20 +0200

    Improve `Package.swift` - Add supported platforms - Add path to avoid demo apps to be packaged

--- Package.swift
+++ Package.swift
@@ -5,24 +5,28 @@ import PackageDescription
 
 let package = Package(
     name: "FortunesAlgorithm",
+    platforms: [
+        .macOS(.v10_12),
+        .iOS(.v10),
+        .tvOS(.v10),
+        .watchOS(.v3)
+    ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "FortunesAlgorithm",
-            targets: ["FortunesAlgorithm"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+            targets: ["FortunesAlgorithm"]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "FortunesAlgorithm",
-            dependencies: []),
+            path: "Sources"
+        ),
         .testTarget(
             name: "FortunesAlgorithmTests",
-            dependencies: ["FortunesAlgorithm"]),
+            dependencies: ["FortunesAlgorithm"]
+        ),
     ]
 )
+
+
