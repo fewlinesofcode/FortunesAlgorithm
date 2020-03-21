b04b9112f16fcb0902eda45ccc450a356e23ae99
Oleksandr Glagoliev <aglagoliev@gmail.com>
2020-03-21 09:30:41 +0200

    Fix Tests path

--- Package.swift
+++ Package.swift
@@ -24,7 +24,8 @@ let package = Package(
         ),
         .testTarget(
             name: "FortunesAlgorithmTests",
-            dependencies: ["FortunesAlgorithm"]
+            dependencies: ["FortunesAlgorithm"],
+            path: "Tests"
         ),
     ]
 )
